d97429e73cc0f6a54df4047cc2235099541f7f71
Jason Wright <jason.wright@possibleworldwide.com>
2011-11-23 14:26:11 -0500

    merge test 2

--- GitTest1/src/wright/git/GitTest1.java
+++ GitTest1/src/wright/git/GitTest1.java
@@ -2,6 +2,6 @@ package wright.git;
 
 public class GitTest1 
 {
-	//This is the test for GitTest1.
+	//This is the (merge test 2) test for GitTest1.
 	//This is the test for GitBranch1 from GitTest1.
 }
